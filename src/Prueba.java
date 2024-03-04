22379c10e042bbdd7b08d889964560be2cb932e6
Numi <mvalenzuelarobledo@safareyes.es>
2024-03-04 09:49:20 +0100

    primer comit de jozelito

--- src/Prueba.java
+++ src/Prueba.java
@@ -2,5 +2,7 @@ public class Prueba {
     public static void main(String[] args) {
         System.out.println("HI! \nI´m Numi <3");
         System.out.println("\t- EED");
+        System.out.println("\n\nMVR");
+
     }
 }
